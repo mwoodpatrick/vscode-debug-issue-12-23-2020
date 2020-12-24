d9ce0576004f3b3c153fc44d0b21ca7640599263
mwoodpatrick <mwoodpatrick@gmail.com>
2020-12-23 17:50:52 -0800

    Pause at start of program

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your name ? ", function(name) {
+  console.log('continuing ${name}');
+});
+
 let i = 0;
 
 console.log("starting program")
